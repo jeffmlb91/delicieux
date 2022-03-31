308649f1d7ef868ec231b5e18403449663e90c85
jeffmlb91 <jeffmlb91@gmail.com>
2022-03-30 23:03:50 -0400

    update veggies.jsx

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -12,11 +12,20 @@ function Popular() {
   },[]);
 
   const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-    const data = await api.json();
-    setPopular(data.recipes);
-    console.log(data.recipes);
-  }
+
+    const check = localStorage.getItem('popular');
+    
+    if(check){
+      setPopular(JSON.parse(check));
+    }else{
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+      const data = await api.json();
+
+      localStorage.setItem('popular', JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+      console.log(data.recipes);
+    }
+  };
   return (
     <div>
          <Wrapper>
@@ -33,7 +42,7 @@ function Popular() {
 
               {popular.map((recipe) => {
                 return(
-                    <SplideSlide>
+                    <SplideSlide key={recipe.id}>
                       <Card>
                         <p>{recipe.title}</p>
                         <img src={recipe.image} alt=""/>
